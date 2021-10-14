2a7dbf7f32beeb94d95b0a26d64fa895b1cb82b2
loueque <louequerodab@gmail.com>
2021-10-13 21:59:17 -0700

    code improvements

---
+++ main.py
@@ -0,0 +1,47 @@
+import random as rng
+
+organizer = "NFL"
+
+# table of NFl teams and division title
+# NFL teams and division title
+
+teams = {
+    "ARI": "Arizona Cardinals",
+    "ATL": "Atlanta Falcons",
+    "BAL": "Baltimore Ravens",
+    "BUF": "Buffalo Bills",
+    "CAR": "Carolina Panthers",
+    "CHI": "Chicago Bears",
+    "CIN": "Cincinnati Bengals",
+    "CLE": "Cleveland Browns",
+    "DAL": "Dallas Cowboys",
+    "DEN": "Denver Broncos",
+    "DET": "Detroit Lions",
+    "GB": "Green Bay Packers",
+    "HOU": "Houston Texans",
+    "IND": "Indianapolis Colts",
+    "JAX": "Jacksonville Jaguars",
+    "KC": "Kansas City Chiefs",
+    "LAC": "Los Angeles Chargers",
+    "LAR": "Los Angeles Rams",
+    "MIA": "Miami Dolphins",
+    "MIN": "Minnesota Vikings",
+    "NE": "New England Patriots",
+    "NO": "New Orleans Saints",
+    "NYG": "New York Giants",
+    "NYJ": "New York Jets",
+    "OAK": "Las Vegas Raiders",
+    "PHI": "Philadelphia Eagles",
+    "PIT": "Pittsburgh Steelers",
+    "SF": "San Francisco 49ers",
+    "SEA": "Seattle Seahawks",
+    "TB": "Tampa Bay Buccaneers",
+    "TEN": "Tennessee Titans",
+    "WAS": "Washington Redskins",
+}
+
+def main():
+    rng_team = rng.choice(list(teams.keys()))
+    rng_team_name = teams[rng_team]
+    print(f"{organizer} {rng_team_name}")
+    
