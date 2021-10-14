bbe8cd9bde941a8f05f9cb6ee242ad6c60d4c026
loueque <louequerodab@gmail.com>
2021-10-13 22:22:56 -0700

    big improvements, shortened code

--- main.py
+++ main.py
@@ -2,61 +2,98 @@ import random as rng
 
 organizer = "NFL"
 
-# table of NFl teams and division title
-# NFL teams and division title
+divisions = {
+    "AFC": {
+        "North": {
+            "CIN": "Cincinnati Bengals",
+            "CLE": "Cleveland Browns",
+            "PIT": "Pittsburgh Steelers",
+            "BAL": "Baltimore Ravens",
+        },
 
-teams = {
-    "ARI": "Arizona Cardinals",
-    "ATL": "Atlanta Falcons",
-    "BAL": "Baltimore Ravens",
-    "BUF": "Buffalo Bills",
-    "CAR": "Carolina Panthers",
-    "CHI": "Chicago Bears",
-    "CIN": "Cincinnati Bengals",
-    "CLE": "Cleveland Browns",
-    "DAL": "Dallas Cowboys",
-    "DEN": "Denver Broncos",
-    "DET": "Detroit Lions",
-    "GB": "Green Bay Packers",
-    "HOU": "Houston Texans",
-    "IND": "Indianapolis Colts",
-    "JAX": "Jacksonville Jaguars",
-    "KC": "Kansas City Chiefs",
-    "LAC": "Los Angeles Chargers",
-    "LAR": "Los Angeles Rams",
-    "MIA": "Miami Dolphins",
-    "MIN": "Minnesota Vikings",
-    "NE": "New England Patriots",
-    "NO": "New Orleans Saints",
-    "NYG": "New York Giants",
-    "NYJ": "New York Jets",
-    "OAK": "Las Vegas Raiders",
-    "PHI": "Philadelphia Eagles",
-    "PIT": "Pittsburgh Steelers",
-    "SF": "San Francisco 49ers",
-    "SEA": "Seattle Seahawks",
-    "TB": "Tampa Bay Buccaneers",
-    "TEN": "Tennessee Titans",
-    "WAS": "Washington Redskins",
+        "East": {
+            "NE": "New England Patriots",
+            "BUF": "Buffalo Bills",
+            "MIA": "Miami Dolphins",
+            "NYJ": "New York Jets",
+        },
+
+        "South": {
+            "HOU": "Houston Texans",
+            "IND": "Indianapolis Colts",
+            "JAX": "Jacksonville Jaguars",
+            "TEN": "Tennessee Titans",
+        },
+
+        "West": {
+            "LAC": "Los Angeles Chargers",
+            "OAK": "Oakland Raiders",
+            "DEN": "Denver Broncos",
+            "KC": "Kansas City Chiefs",
+        }
+    },
+
+    "NFC": {
+        "North": {
+            "GB": "Green Bay Packers",
+            "MIN": "Minnesota Vikings",
+            "DET": "Detroit Lions",
+            "CHI": "Chicago Bears",
+        },
+
+        "East": {
+            "NYG": "New York Giants",
+            "PHI": "Philadelphia Eagles",
+            "WAS": "Washington Redskins",
+            "DAL": "Dallas Cowboys",
+        },
+
+        "South": {
+            "ATL": "Atlanta Falcons",
+            "CAR": "Carolina Panthers",
+            "NO": "New Orleans Saints",
+            "TB": "Tampa Bay Buccaneers",
+        },
+
+        "West": {
+            "ARI": "Arizona Cardinals",
+            "LAR": "Los Angeles Rams",
+            "SF": "San Francisco 49ers",
+            "SEA": "Seattle Seahawks",
+        },
+    }
 }
 
+def print_team():
+    rng_conf = rng.choice(list(divisions.keys()))
+    rng_div = rng.choice(list(divisions[rng_conf].keys()))
+
+    return rng_conf, rng_div
+
+print(print_team())
+
 def play_game():
-    rng_team = rng.choice(list(teams.keys()))
-    rng_team_one = teams[rng_team]
-    rng_team_ = rng.choice(list(teams.keys()))
-    rng_team_two = teams[rng_team_]
-    print(f"{rng_team_one} vs {rng_team_two}")
+    # using print_team(), chose out a random team in the conference and division
+    conf, div = print_team()
+    rng_team_one = rng.choice(list(divisions[conf][div].values()))
+    rng_team_conf_one = rng.choice(list(divisions[conf].values()))
+    rng_team_two = rng.choice(list(divisions[conf][div].values()))
+    rng_team_conf_two = rng.choice(list(divisions[conf].values()))
+
+    # print the teams
+    print(f"{rng_team_one} vs. {rng_team_two}")
     print("Kickoff!")
-    score_one = rng.randint(0, 70)
-    score_two = rng.randint(0, 70)
+
+    score_one = rng.randint(0, 52)
+    score_two = rng.randint(0, 52)
     print(f"{rng_team_one} scored {score_one}, and {rng_team_two} scored {score_two}")
     if score_one > score_two:
         if score_two == 0:
-            print(f"{rng_team_two} had a shutdown against {rng_team_one}!")
+            print(f"{rng_team_two} from {rng_team_conf_two} had a shutdown against {rng_team_one} from {rng_team_conf_one}!")
         print(f"{rng_team_one} wins!")
     elif score_one < score_two:
         if score_one == 0:
-            print(f"{rng_team_two} had a shutout against {rng_team_one}!")
+            print(f"{rng_team_two} from {rng_team_conf_two} had a shutdown against {rng_team_one} from {rng_team_conf_one}!")
         print(f"{rng_team_two} wins!")
 
 def main():
