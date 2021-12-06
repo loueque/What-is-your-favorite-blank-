431cb6e082c02a44c7953586cb03b88732ff50b7
loueque <louequerodab@gmail.com>
2021-12-05 20:31:17 -0800

    c+ implementation

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,1 +1,13 @@
+#include <random>
+#include <string>
+#include <map>
 
+class Organization {
+	public:
+		int team_numbers = 32;
+		std::string organizer = "NFL";
+};
+
+int main() {
+	return 0;
+}
